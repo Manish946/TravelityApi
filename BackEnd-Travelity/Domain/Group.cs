8c9f2519f71c46c98057150b270477320196804c
Manish946 <50454163+Manish946@users.noreply.github.com>
2022-05-19 12:00:01 +0200

    sql database Updated.

--- BackEnd-Travelity/Domain/Group.cs
+++ BackEnd-Travelity/Domain/Group.cs
@@ -8,17 +8,16 @@ namespace BackEnd_Travelity.Domain
         [Key]
         public int Id { get; set; }
         [Required]
-        public string GroupName { get; set; }
-        public string Destination { get; set; }
+        public string Group_name { get; set; }
+        public string Group_description { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM dd yyyy}")]
-        public string GroupAdmin { get; set; }
-        public string GroupThumbnail { get; set; }          
-        public DateTime CreatedTimeStamp { get; set; }
+        public DateTime Date { get; set; }
         public ICollection<User> Users { get; set; }
         public ICollection<Picture> Pictures { get; set; }
         public ICollection<Admin> Admins { get; set; }
         public ICollection<Chat> Chats { get; set; }
         public ICollection<Place> Places { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
